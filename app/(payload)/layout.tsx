22338dbe63c5d718be0739bca86c8041f9b4a70a
Kamil Kowalczuk <kamil.kowalczuk17@gmail.com>
2026-05-05 13:02:24 +0200

    Fix payload restart loop by using image default standalone command

--- app/(payload)/layout.tsx
+++ app/(payload)/layout.tsx
@@ -3,11 +3,16 @@ import type { ReactNode } from 'react';
 import config from '../../payload.config';
 import { importMap } from './admin/importMap';
 
+const serverFunction: typeof handleServerFunctions = async (...args) => {
+    'use server';
+    return handleServerFunctions(...args);
+};
+
 export default function Layout({ children }: { children: ReactNode }) {
     return RootLayout({
         children,
         config: Promise.resolve(config),
         importMap,
-        serverFunction: handleServerFunctions
+        serverFunction
     });
 }
