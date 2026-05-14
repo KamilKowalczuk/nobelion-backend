5d797a107aca6d4b860422dbcf47ee70cd440c98
Kamil Kowalczuk <kamil.kowalczuk17@gmail.com>
2026-05-14 11:11:25 +0200

    Design porpawa

--- payload.config.ts
+++ payload.config.ts
@@ -1,12 +1,10 @@
 import { buildConfig } from 'payload';
 import { postgresAdapter } from '@payloadcms/db-postgres';
-import { lexicalEditor } from '@payloadcms/richtext-lexical';
 import { Briefs } from './src/collections/Briefs';
 import { Orders } from './src/collections/Orders';
 import { Users } from './src/collections/Users';
 
 export default buildConfig({
-    editor: lexicalEditor({}),
     secret: process.env.PAYLOAD_SECRET || 'replace-me',
     cors: ['https://nobelion.pl', 'https://www.nobelion.pl', 'https://admin.nobelion.pl', 'http://localhost:4321', 'http://localhost:3000', 'http://localhost:3001'],
     csrf: ['https://nobelion.pl', 'https://www.nobelion.pl', 'https://admin.nobelion.pl', 'http://localhost:4321', 'http://localhost:3000', 'http://localhost:3001'],
